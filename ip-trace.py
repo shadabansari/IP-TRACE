0338ec0bd18959b5ba0587561b47abac780b52d8
Shadab Ansari <ansari.shadab1992@gmail.com>
2018-08-31 01:05:04 +0530

    Update ip-trace.py

--- ip-trace.py
+++ ip-trace.py
@@ -1,13 +1,12 @@
-import os
 import requests
 import json
 
 pattern = [('.|.'*(2*i + 1)).center(33, '-') for i in range(5) ]
-print('\n'.join(pattern + ['IP-TRACER CODED BY SHADAB'.center(33, '-')] + pattern[::-1]))
+print('\n'.join(pattern + ['IP-TRACER'.center(33, '-')] + pattern[::-1]))
 
 url = 'http://ip-api.com/json/'
 ip = input('Enter IP Address : ')
-r = requests.get(url, ip)
+r = requests.get(url+ip)
 data = r.json()
 
 print ('\nIP : ' + data['query'])
