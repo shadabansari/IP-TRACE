b9d1542e3c4b078685a36dc1f022fefca4268aa3
Shadab Ansari <ansari.shadab1992@gmail.com>
2018-08-30 20:32:12 +0530

    Add files via upload

---
+++ ip-trace.py
@@ -0,0 +1,22 @@
+import os
+import requests
+import json
+
+pattern = [('.|.'*(2*i + 1)).center(33, '-') for i in range(5) ]
+print('\n'.join(pattern + ['IP-TRACER CODED BY SHADAB'.center(33, '-')] + pattern[::-1]))
+
+url = 'http://ip-api.com/json/'
+ip = input('Enter IP Address : ')
+r = requests.get(url, ip)
+data = r.json()
+
+print ('\nIP : ' + data['query'])
+print ('Status : ' + data['status'])
+print ('Region : ' + data['regionName'])
+print ('City : ' + data['city'])
+print ('Country : ' + data['country'])
+print ('ISP : ' + data['isp'])
+print ('Lat, Lon : ' + str(data['lat']) + '  ' + str(data['lon']))
+print ('ZIPCODE : ' + data['zip'])
+print ('Timezone : ' + data['timezone'])
+print ('AS : ' + data['as'])
